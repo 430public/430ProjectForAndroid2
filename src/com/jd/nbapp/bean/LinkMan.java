f8aac29cb9f232eed8afc16806a2349ada8ea417
gotlon <Golton@outlook.com>
2015-10-18 21:47:24 +0800

    映射到数据库的实体类

---
+++ src/com/jd/nbapp/bean/LinkMan.java
@@ -0,0 +1,50 @@
+package com.jd.nbapp.bean;
+
+public class LinkMan {
+	private String name;
+	private String phone;
+	private String address;
+	
+	public LinkMan() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 实例化联系人
+	 * @param name 联系人
+	 * @param phone 电话
+	 * @param address 地址
+	 */
+	public LinkMan(String name, String phone, String address) {
+		super();
+		this.name = name;
+		this.phone = phone;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
